1df0184ccb6b2f536724624e2d9b121faaf74681
Kyle Hill <kylehill@gmail.com>
2014-12-22 18:34:25 -0500

    Front-end complete

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-console.log("testing")
+(function(window){
+  
+  Nomencolor.load(colorList)
+  
+  var translateWrapper = function(colorText) {
+    var translated = Nomencolor.compare(colorText)
+    
+    $("#swatches").empty()
+    
+    _.each(translated.nearest, function(color) {
+      $("#swatches").append($("<div data-hex='" + (color.hex || color.color.hex) + "'></div>")
+                    .addClass("swatch")
+                    .css("background-color", color.hex || color.color.hex))
+    })
+    
+    var closest = _.first(translated.nearest).color || _.first(translated.nearest)
+    
+    $("#name").text(closest.name)
+    $("#hex").text(closest.hex)
+    $("#rgb").text(["rgb(", closest.rgb.r, ", ", closest.rgb.g, ", ", closest.rgb.b, ")"].join(""))
+    $("#hsl").text(["hsl(", closest.hsl.h, ", ", closest.hsl.s, "%, ", closest.hsl.l, "%)"].join(""))
+    
+    $("#feature-swatch").css("background-color", closest.hex)
+    
+    $(".swatch").on("click", function(evt){
+      translateWrapper($(evt.target).attr("data-hex"))
+    })
+  }
+  
+  translateWrapper("orangered")
+  
+  window.translateWrapper = translateWrapper
+  
+})(window);
--- nomencolor.js
+++ nomencolor.js
@@ -1,6 +1,6 @@
 (function(window){
   
-  var computeColor = function(colorText) {
+  function computeColor(colorText) {
     var container = document.getElementById("colorCompute")
     var div = document.createElement("div")
     container.appendChild(div)
@@ -12,7 +12,7 @@
     return color
   }
   
-  var convertToHsl = function(r1, g1, b1) {
+  function convertToHsl(r1, g1, b1) {
     var r = r1/255, g = g1/255, b = b1/255
     
     var min = Math.min(r,g,b)
@@ -49,37 +49,92 @@
     }
     
     return {
-      h: h,
-      s: s,
-      l: l
+      h: Math.round(h),
+      s: Math.round(s * 100),
+      l: Math.round(l * 100)
     }
   }
   
-  var nomencolor = function(colorText){
+  function convertToHex(r1, g1, b1) {
+    var r = r1.toString(16), g = g1.toString(16), b = b1.toString(16)
+    
+    if (r.length === 1) {
+      r = ("0" + r)
+    }
+    if (g.length === 1) {
+      g = ("0" + g)
+    }
+    if (b.length === 1) {
+      b = ("0" + b)
+    }
+    
+    return "#" + r + g + b
+  }
+  
+  function translate(colorText, name) {
     
     // Convert input from x11/rgb/hex/hsl to rgb/rgba
-    var computedRgbString = computeColor(colorText)
+    var rgbString = computeColor(colorText)
     
     // Convert rgb(a) string to numbers
-    var computedRgbValues = computedRgbString.split("(")[1].split(")")[0].split(",").map(function(i){ return i * 1 })
-    
-    // Convert rgb to hsl
-    var computedHslValues = convertToHsl(computedRgbValues[0], computedRgbValues[1], computedRgbValues[2])
+    var rgb = rgbString.split("(")[1].split(")")[0].split(",").map(function(i){ return i * 1 })
     
     var output = {
-      input: colorText,
       rgb: {
-        r: computedRgbValues[0],
-        g: computedRgbValues[1],
-        b: computedRgbValues[2]
+        r: rgb[0],
+        g: rgb[1],
+        b: rgb[2]
       },
-      hsl: computedHslValues
+      hsl: convertToHsl(rgb[0], rgb[1], rgb[2]),
+      hex: convertToHex(rgb[0], rgb[1], rgb[2])
+    }
+    
+    if (name) {
+      output.name = name
     }
     
     return output
     
   }
   
-  window.Nomencolor = nomencolor
+  var colorMemory = []
+  
+  function load(colorList) {
+    colorList.forEach(function(color){
+      colorMemory.push(translate("#" + color[0], color[1]))
+    })
+  }
+  
+  function compare (colorText) {
+    var t = translate(colorText)
+    
+    var comparisons = colorMemory.map(function(color) {
+      var rgbDelta = Math.pow(t.rgb.r - color.rgb.r, 2) + Math.pow(t.rgb.g - color.rgb.g, 2) + Math.pow(t.rgb.b - color.rgb.b, 2)
+      var hDelta = Math.min(Math.abs(t.hsl.h - color.hsl.h), Math.abs(t.hsl.h - (color.hsl.h + 360)))
+      
+      var hslDelta = Math.pow(hDelta, 2) + Math.pow(t.hsl.s - color.hsl.s, 2) + Math.pow(t.hsl.l - color.hsl.l, 2)
+      
+      return {
+        color: color,
+        delta: (rgbDelta * 200) + (hslDelta * 10)
+      }
+    })
+    
+    comparisons.sort(function(a, b) {
+      return a.delta - b.delta
+    })
+    
+    return {
+      input: t,
+      nearest: comparisons.slice(0, 20)
+    }
+  }
+  
+  window.Nomencolor = {
+    colors: colorMemory,
+    load: load,
+    translate: translate,
+    compare: compare
+  }
   
 })(window);
