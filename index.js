dc0e03b8743fdb35a0dced4c600d35ec087b3187
Kyle Hill <kylehill@gmail.com>
2014-12-22 20:05:39 -0500

    Random starter color and hover state

--- index.js
+++ index.js
@@ -2,33 +2,53 @@
   
   Nomencolor.load(colorList)
   
-  var translateWrapper = function(colorText) {
+  var randomHex = function() {
+    return "#" + Math.floor(Math.random() * 16).toString(16)
+      + Math.floor(Math.random() * 16).toString(16)
+      + Math.floor(Math.random() * 16).toString(16)
+  }
+  
+  window.translateWrapper = function(colorText) {
     var translated = Nomencolor.compare(colorText)
     
     $("#swatches").empty()
     
-    _.each(translated.nearest, function(color) {
-      $("#swatches").append($("<div data-hex='" + (color.hex || color.color.hex) + "'></div>")
-                    .addClass("swatch")
-                    .css("background-color", color.hex || color.color.hex))
+    _.each(translated.nearest, function(color, i) {
+      $("#swatches").append($("<div></div>")
+        .addClass("swatch")
+        .attr("data-hex", color.color.hex)
+        .attr("data-index", i)
+        .css("background-color", color.color.hex))
     })
     
-    var closest = _.first(translated.nearest).color || _.first(translated.nearest)
-    
-    $("#name").text(closest.name)
-    $("#hex").text(closest.hex)
-    $("#rgb").text(["rgb(", closest.rgb.r, ", ", closest.rgb.g, ", ", closest.rgb.b, ")"].join(""))
-    $("#hsl").text(["hsl(", closest.hsl.h, ", ", closest.hsl.s, "%, ", closest.hsl.l, "%)"].join(""))
-    
-    $("#feature-swatch").css("background-color", closest.hex)
+    var closest = _.first(translated.nearest).color
+
+    updateMain(closest)
     
     $(".swatch").on("click", function(evt){
       translateWrapper($(evt.target).attr("data-hex"))
     })
+    
+    $(".swatch").on("mouseover", function(evt){
+      var index = $(evt.target).attr("data-index")
+      updateMain(translated.nearest[index].color)
+    })
+    
+    $(".swatch").on("mouseout", function(evt){
+      updateMain(translated.nearest[0].color)
+    })
   }
   
-  translateWrapper("orangered")
+  window.updateMain = function(color) {
+    $("#name").text(color.name)
+    $("#hex").text(color.hex)
+    $("#rgb").text(["rgb(", color.rgb.r, ", ", color.rgb.g, ", ", color.rgb.b, ")"].join(""))
+    $("#hsl").text(["hsl(", color.hsl.h, ", ", color.hsl.s, "%, ", color.hsl.l, "%)"].join(""))
+    
+    $("#feature-swatch").css("background-color", color.hex)
+    
+  }
   
-  window.translateWrapper = translateWrapper
+  translateWrapper(randomHex())
   
 })(window);
